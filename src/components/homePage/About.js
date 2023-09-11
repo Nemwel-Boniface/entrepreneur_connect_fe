09f70e9020a9790f527ea7617bdb193c5bc96831
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-11 23:29:31 +0300

    Update added some files to be added to the home page in prep to test React router

---
+++ src/components/homePage/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="AboutUsComponent">
+      This is the AboutUs component
+    </div>
+  )
+};
+
+export default AboutUs;
---
+++ src/components/homePage/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div className="ContactUsComponent">
+      This is the ContactUs component
+    </div>
+  )
+};
+
+export default ContactUs;
