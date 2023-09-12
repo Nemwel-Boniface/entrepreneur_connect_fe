0b3e2321bb662bda7f2926944f134ba6f4fa4515
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-12 22:04:13 +0300

    Update included:   - Added font awesome fonts, updated project title and added cdn for poppins font   - Added some standard styling to be fprward with custom CSS and not use Tailwind anymore

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/homePage/Home';
 import LogIn from './components/user/LogIn';
 import SignUp from './components/user/SignUp';
-import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className='Home'>
-      <NavBar />
       <Routes>
         <Route exact path='/' element={ <Home /> } />
         <Route path='login' element={ <LogIn /> } />
