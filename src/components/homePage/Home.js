137648a6fc4336d902548fc17482840106865bae
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-13 22:48:54 +0300

    Update included:   - Created the markup for the features section   - Created an array with the different features data to render them dynamically

--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroBg from "./Hero";
+import Features from "./Features";
 
 const Home = () => {
   return (
     <div className="HomeComponent">
       <HeroBg />
+      <Features />
     </div>
   )
 };
