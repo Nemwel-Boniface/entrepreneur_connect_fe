55c54d1e3a77ff8e0ea5bfaba2566ca6359f388f
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-15 21:53:29 +0300

    Update included:   - Installed the React icons package to the project   - Created the markup for the Contact us section   - Linked the contact us section to the project

--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import HeroBg from "./Hero";
 import Features from "./Features";
 import AboutUs from "./About";
+import ContactUs from "./ContactUs";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroBg />
       <AboutUs />
       <Features />
+      <ContactUs />
     </div>
   )
 };
