d332fbea3ef2e13844907e2e4cd9febf79c20c14
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-19 12:27:37 +0300

    Update created the signUp slice

---
+++ src/redux/user/signUpSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  loading: false,
+  error: null,
+  success: false,
+}
+
+const signUpSlice = {
+  name: "User signup",
+  initialState,
+  reducers: {
+    signUpRequest: (state) => {
+      state.loading = true;
+      state.error = null
+    },
+    signUpSuccess: (state) => {
+      state.loading = false;
+      state.success = true;
+    },
+    signUpFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    }
+  }
+};
+
+export const { signUpRequest, signUpSuccess, signUpFailure } = signUpSlice.actions;
+
+export default signUpSlice.reducers;
