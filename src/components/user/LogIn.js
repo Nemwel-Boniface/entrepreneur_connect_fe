bfd28a9b63c7c0d4524e5cd9864009ef4d068b39
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-21 13:06:24 +0300

    Update included:   - Updated the redux store config to include the loginslice   - Add the user login functionality to the LogIn component dispatching actions for the following based on API responses:     - login request     - login success     - login failure

--- src/components/user/LogIn.js
+++ src/components/user/LogIn.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logInRequest, logInSuccess, logInFailure } from '../../redux/user/logInSlice';
 
-const LogIn = () => (
-  <div className="LogInComponent">
-    This is the LogIn component
-  </div>
-);
+const LogIn = () => {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.logIn.loading);
+  const success = useSelector((state) => state.logIn.success);
+  const failure = useSelector((state) => state/logIn.failure);
+  const isLoggesIn = useSelector((state) => state.logIn.isLoggedIn);
 
+  const [ formData, setFormData ] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(logInRequest());
+
+    try {
+      const response = await fetch("API", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData)
+      })
+
+      if(!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message)
+        console.log(errorData.message);
+      }
+
+      const responseData = await response.json();
+      const authToken = responseData.access;
+      console.log("response data" + authToken)
+
+      localStorage.setItem("authToken", authToken);
+
+      dispatch(logInSuccess(authToken));
+    } catch(error) {
+      dispatch(logInFailure(error.message));
+    }
+  }
+}
 export default LogIn;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import signUpSlice from "./user/signUpSlice";
+import logInSlice from "./user/logInSlice";
 
 const store = configureStore({
   reducer: {
     signUp: signUpSlice,
+    logIn: logInSlice,
   }
 })
 
