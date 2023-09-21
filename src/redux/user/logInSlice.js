9a282ab941a4fb97242a512e9cb0b2a85926dbbb
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-21 12:18:28 +0300

    Update included:   - Created the login slice creating action creators for:     - Sending the login Request     - Checking for login success     - Checking for login failure     - Logging out   - Exported the actions as

---
+++ src/redux/user/logInSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  loading: false,
+  success: false,
+  error: null,
+  token: null,
+  isLoggedIn: false,
+};
+
+const logInSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    logInRequest: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    logInSuccess: (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.token = action.payload;
+      state.isLoggedIn = true;
+    },
+    logInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.isLoggedIn = false;
+    },
+    logOut: (state) => {
+      state.success = false;
+      state.isLoggedIn = false;
+      state.token = null;
+    }
+  }
+})
+
+export const { logInRequest, logInSuccess, logInFailure, logOut } = logInSlice.actions;
+
+export default logInSlice.reducer;
