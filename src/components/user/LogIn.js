4271d7d2cbda79d2864be8285bda154e6e2b5b91
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-21 13:53:57 +0300

    Update included:   - Included the login form that takes email and password   - Created a test "isloggedin" to try to mock the logout functionality

--- src/components/user/LogIn.js
+++ src/components/user/LogIn.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { logInRequest, logInSuccess, logInFailure } from '../../redux/user/logInSlice';
+import { logInRequest, logInSuccess, logInFailure, logOut } from '../../redux/user/logInSlice';
 
 const LogIn = () => {
   const dispatch = useDispatch();
   const loading = useSelector((state) => state.logIn.loading);
   const success = useSelector((state) => state.logIn.success);
-  const failure = useSelector((state) => state/logIn.failure);
-  const isLoggesIn = useSelector((state) => state.logIn.isLoggedIn);
+  const failure = useSelector((state) => state.logIn.failure);
+  const isLoggedIn = useSelector((state) => state.logIn.isLoggedIn);
 
   const [ formData, setFormData ] = useState({
     email: '',
@@ -19,7 +19,7 @@ const LogIn = () => {
     dispatch(logInRequest());
 
     try {
-      const response = await fetch("API", {
+      const response = await fetch("http://127.0.0.1:8000/api/user/login/", {
         method: "POST",
         headers: {
           "content-type": "application/json",
@@ -44,5 +44,55 @@ const LogIn = () => {
       dispatch(logInFailure(error.message));
     }
   }
+
+  const handleLogOut = () => {
+    dispatch(logOut());
+    console.log(isLoggedIn)
+  }
+
+  const renderLoginForm = () => {
+    if (isLoggedIn) {
+      // If the user is logged in, render a different component or redirect to a different page
+      return (<div>
+        <h2>You are logged in!</h2>
+        <button type="submit" onClick={handleLogOut}>
+            LogOut
+          </button>
+      </div>);
+
+    } else {
+      return (
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Your Email"
+            value={formData.email}
+            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          />
+
+          <button type="submit" disabled={loading}>
+            Login
+          </button>
+
+          {loading && <p>Logging in...</p>}
+          {failure && <p>Error: {error}</p>}
+          {success && <p>Login successful!</p>}
+        </form>
+      );
+    }
+  };
+
+  return (
+    <section className="loginComponent">
+      {renderLoginForm()}
+    </section>
+  );
 }
 export default LogIn;
