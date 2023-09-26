ddb7bea2c01c6045020e7beb26897ebb22b6d439
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-26 23:20:56 +0300

    Update included:   - Created the Error 404 markup   - Added the image to be used in the Error 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,20 @@
 import React from "react";
+import hamster from '../images/hamster.png'
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <section className="Error404Page">
-      <div className="not found">Not found</div>
+    <section className="NotFound">
+      <div className="NotFoundLeft">
+        <img src={hamster} alt="" />
+      </div>
+      <div className="NotFoundRight">
+        <h1>Shhh...</h1>
+        <p>Hold up a sec! Our hamster friend is catching Z's. Tip - toe back to the zoo for more wild fun.</p>
+        <button>
+          <Link to="/"><i class="fa fa-arrow-left" aria-hidden="true"></i>This way</Link>
+        </button>
+      </div>
     </section>
   )
 }
