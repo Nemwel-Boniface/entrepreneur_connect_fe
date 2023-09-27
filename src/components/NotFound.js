bc7715b2e440c025031dc0745f66d36c2eb5fe78
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-09-27 21:39:33 +0300

    Update to the Error 404 message

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -9,8 +9,11 @@ const NotFound = () => {
         <img src={hamster} alt="" />
       </div>
       <div className="NotFoundRight">
-        <h1>Shhh...</h1>
-        <p>Hold up a sec! Our hamster friend is catching Z's. Tip - toe back to the zoo for more wild fun.</p>
+        <h1>Shhh...Error 404!</h1>
+        <p>Oops, it seems our entrepreneurial hamster took a coffee break ☕️ and wandered off to brainstorm its next big idea! 🐹💡</p>
+        <p>While our little genius is away, why not explore our other wild pages? Who knows, you might stumble upon your next Eureka moment! 🚀✨</p>
+        <p>While our entrepreneurial hamster naps, here's some wisdom: 'Happiness is the key to success. Love what you do, and you'll find it.' 🌟</p>
+        <p>Feel free to use the map or search bar above to navigate back to the action, and if you find our entrepreneurial hamster, be sure to let us know! 🐹🕵️‍♂️ #HamsterHunt</p>
         <button>
           <Link to="/"><i class="fa fa-arrow-left" aria-hidden="true"></i>This way</Link>
         </button>
