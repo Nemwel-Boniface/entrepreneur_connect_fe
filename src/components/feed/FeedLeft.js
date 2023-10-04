d9ca2b15e1c0c2bf3f8dcdd67cd3aa2714e40c9f
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-10-04 12:21:19 +0300

    Update included:   - Installed the React calendar to the project   - Used the React calendar in the project to get the date   - Added some styling to the feed right and left sections to distinguish them from the main feed

--- src/components/feed/FeedLeft.js
+++ src/components/feed/FeedLeft.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 
 const FeedLeft = () => {
+  const [ date, setDate ] = useState(new Date());
   return (
     <section className="feedLeft">
       <h2>The Feed left section</h2>
+      
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
     </section>
   )
 }
