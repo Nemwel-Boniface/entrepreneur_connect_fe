f7dc9579291549923e8d48b3d524bc2341c0cf2d
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-10-30 21:44:55 +0300

    Update created the createComments function

--- src/redux/comment/commentSlice.js
+++ src/redux/comment/commentSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const createCommentsURL = `http://127.0.0.1:8000/api/posts/comments/`;
 
 const initialState = {
   comment: [],
@@ -6,6 +8,32 @@ const initialState = {
   isError: false,
   status: "null",
 }
+
+export const createComment = createAsyncThunk('comments/postComments', async (comment) => {
+  try {
+    const formData = {
+      comment: comment.text,
+      post: comment.id,
+    }
+
+    const response = await fetch(createCommentsURL, {
+      method: "POST",
+      headers: {
+        'content_type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+      },
+      body: JSON.stringify(formData)
+    })
+
+    if(!response.ok) {
+      throw new Error(`Failed to create comment`)
+    }
+    const addedComment = response.json()
+    return addedComment;
+  } catch(err) {
+    return `Failed to create comment ${err.message}`
+  }
+})
 
 const commentSlice = createSlice({
   name: "comments",
